35ebbc62eddded5a8c7768766f6120dba3701af9
Pedro Markun <pedro@esfera.mobi>
2013-04-09 10:55:25 -0300

    Adicionado sistema de cache. O código pode usar um refatoramento basico... cache setado pra 1 em 1 min

---
+++ data/readme.md
@@ -0,0 +1,2 @@
+# Remember!
+Set write permission on this dir!
--- projects.php
+++ projects.php
@@ -1,18 +1,35 @@
 <?php
 if(isset($_GET['id'])) {
 	// retrieve single project
-	$project = json_decode(file_get_contents('http://www.texugo.com.br/bxc2013/mapa!projeto.action?codProjeto=' . $_GET['id']), true);
-	$output = $project['values'];
-	$output['data'] = fixDate($output['data']);
+	$cache_file = "data/project_" . $_GET['id'] . ".json";
+	if (file_exists($cache_file) && (filemtime($cache_file) > (time() - 60))) {
+		$output = file_get_contents($cache_file);
+	}
+	else {
+		$project = json_decode(file_get_contents('http://www.texugo.com.br/bxc2013/mapa!projeto.action?codProjeto=' . $_GET['id']), true);
+		$output = $project['values'];
+		$output['data'] = fixDate($output['data']);
+		$output = json_encode($output);
+		file_put_contents($cache_file, $output, LOCK_EX);
+	}
 } else {
 	// retrieve all projects
-	$projects = json_decode(file_get_contents('http://www.texugo.com.br/bxc2013/mapa!pins.action'), true);
-	$output = $projects['values'];
-	// fix date
-	foreach($output as &$item) {
-		error_log($item['data']);
-		$item['data'] = fixDate($item['data']);
+	$cache_file = "data/projects.json";
+	if (file_exists($cache_file) && (filemtime($cache_file) > (time() - 60))) {
+		$output = file_get_contents($cache_file);
 	}
+	else {
+		$projects = json_decode(file_get_contents('http://www.texugo.com.br/bxc2013/mapa!pins.action'), true);
+		$output = $projects['values'];
+		// fix date
+		foreach($output as &$item) {
+			error_log($item['data']);
+			$item['data'] = fixDate($item['data']);
+		}
+		$output = json_encode($output);
+		file_put_contents($cache_file, $output, LOCK_EX);
+	}
+
 }
 
 function fixDate($date) {
@@ -40,6 +57,6 @@ function fixDate($date) {
 }
 
 header('Content-type: application/json');
-echo json_encode($output);
+echo $output;
 exit;
 ?>
